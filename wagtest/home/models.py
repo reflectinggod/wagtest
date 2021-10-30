99208f4da0e77c17a03b61018b0b6859cfdbb964
rbobot <rplant2020@gmail.com>
2021-10-30 10:22:18 +0300

    C002. Add rtf. Add image

--- wagtest/home/models.py
+++ wagtest/home/models.py
@@ -1,7 +1,34 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.core.fields import RichTextField
 
 
 class HomePage(Page):
-    pass
+    # поля в БД
+    subtitle = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name='Подзаголовок'
+    )
+    rtfbody = RichTextField(
+        blank=True,
+        null=True
+    )
+    bg_image = models.ForeignKey(
+        'wagtailimages.Image',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    # поля в админке
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('rtfbody'),
+        ImageChooserPanel('bg_image')
+    ]
